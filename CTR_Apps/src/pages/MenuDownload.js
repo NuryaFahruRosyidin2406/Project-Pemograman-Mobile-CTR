1151791bf01212db0f70d56e8926abd6960baff6
NuryaFahruRosyidin2406 <81367105+NuryaFahruRosyidin2406@users.noreply.github.com>
2022-01-05 15:59:00 +0700

    Update Tulisan di menu download, recent, favorites

--- CTR_Apps/src/pages/MenuDownload.js
+++ CTR_Apps/src/pages/MenuDownload.js
@@ -1,38 +1,43 @@
 import React from 'react';
-import {StyleSheet, Text, View,  ScrollView,
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
   Image,
-  ImageBackground,} from 'react-native';
+  ImageBackground,
+} from 'react-native';
 
 const MenuRecent = ({navigation}) => {
   return (
     <>
-    <View style={{flex: 1 }}>
-    <View style={{backgroundColor: '#383232', flexDirection: 'row', justifyContent: 'space-around', padding: 10, }}>
-      <Text style={{ color: 'white', fontWeight: 'bold' }}>Ini Menu Download</Text>
-    </View>
-    <View style={{flex: 1 }}>
-      <Image
-          source={require('../image/image28.png')}
-          style={{height: 200, width: 401}}>
-        </Image>
-
-        <View style={{alignItems: 'center', paddingTop: 10}}>
+      <View style={{flex: 1}}>
+        <View
+          style={{
+            backgroundColor: '#383232',
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            padding: 10,
+          }}>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>Download</Text>
+        </View>
+        <View style={{flex: 1}}>
+          <Image
+            source={require('../image/image28.png')}
+            style={{height: 200, width: 401}}></Image>
+          <View style={{alignItems: 'center', paddingTop: 10}}>
             <Image
               source={require('../image/image14.png')}
               style={{height: 190, width: 400, alignItems: 'center'}}></Image>
           </View>
-
           <View>
-        <ImageBackground
-          source={require('../image/image16.png')}
-          style={{height: 240, width: 401}}></ImageBackground>
+            <ImageBackground
+              source={require('../image/image16.png')}
+              style={{height: 240, width: 401}}></ImageBackground>
           </View>
-
+        </View>
       </View>
-      </View>
-      </>
-  
-
+    </>
   );
 };
 
