1511917330a57a9360d4105f98ad8b356d49f2ec
NuryaFahruRosyidin2406 <81367105+NuryaFahruRosyidin2406@users.noreply.github.com>
2022-01-13 00:40:53 +0700

    Update MenuBaca.js

--- CTR_Apps/src/pages/MenuBaca.js
+++ CTR_Apps/src/pages/MenuBaca.js
@@ -1,190 +1,309 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity,Image,ScrollView,Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Button,
+} from 'react-native';
 
 const MenuBaca = ({navigation}) => {
   return (
-    <ScrollView style={{flex: 1,backgroundColor:'white'}}>
+    <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/1.png')} style={{width:420,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/1.png')}
+          style={{width: 420, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/2.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/2.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/3.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/3.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/4.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/4.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/5.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/5.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/6.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/6.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/7.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/7.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/8.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/8.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/9.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/9.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/10.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/10.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/11.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/11.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/12.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/12.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/13.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/13.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/14.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/14.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/15.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/15.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/16.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/16.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/17.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/17.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/18.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/18.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/19.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/19.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/20.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/20.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/21.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/21.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/22.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/22.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/23.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/23.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/24.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/24.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/25.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/25.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/26.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/26.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/27.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/27.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/28.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/28.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/29.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/29.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/30.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/30.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/31.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/31.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/32.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/32.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/33.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/33.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/34.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/34.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/35.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/35.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/36.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/36.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/37.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/37.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/38.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/38.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/39.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/39.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/40.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/40.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/41.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/41.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/42.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/42.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/43.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/43.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/44.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/44.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/45.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/45.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/46.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/46.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/47.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/47.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/49.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/49.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/50.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/50.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/51.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/51.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/52.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/52.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
       <View>
-      <Image source={require('../image/SakamotoDaysChapter01/53.png')} style={{width:425,height:700}} ></Image>
+        <Image
+          source={require('../image/SakamotoDaysChapter01/53.png')}
+          style={{width: 425, height: 700}}></Image>
       </View>
-      
-      <View style={{paddingTop:5}}>
-              <View style={{paddingTop:5,resizeMode: "contain",
-            height: 50,
-            width: 300}}>
-              <Button
-                title="Back"
-                color="#211D1D"
-                onPress={() => navigation.navigate('MenuDeskripsi')}
-              />
-              </View>
-            </View>
 
-            <View style={{paddingTop:10 ,paddingLeft:100}}>
-              <View style={{paddingTop:5,resizeMode: "contain",
+      <View style={{paddingTop: 5}}>
+        <View
+          style={{
+            paddingTop: 5,
+            resizeMode: 'contain',
             height: 50,
-            width: 300}}>
-              <Button
-                title="Next"
-                color="#211D1D"
-                onPress={() => navigation.navigate('')}
-              />
-              </View>
-            </View>
+            width: 300,
+          }}>
+          <Button
+            title="Back"
+            color="#211D1D"
+            onPress={() => navigation.navigate('MenuDeskripsi')}
+          />
+        </View>
+      </View>
 
+      <View style={{paddingTop: 10, paddingLeft: 100}}>
+        <View
+          style={{
+            paddingTop: 5,
+            resizeMode: 'contain',
+            height: 50,
+            width: 300,
+          }}>
+          <Button
+            title="Next"
+            color="#211D1D"
+            onPress={() => navigation.navigate('')}
+          />
+        </View>
+      </View>
     </ScrollView>
   );
 };
