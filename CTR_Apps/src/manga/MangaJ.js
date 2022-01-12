ba6a85603af8ef457560a88f1b56e7cf71db617e
NuryaFahruRosyidin2406 <81367105+NuryaFahruRosyidin2406@users.noreply.github.com>
2022-01-12 22:10:41 +0700

    Update Manga

--- CTR_Apps/src/manga/MangaJ.js
+++ CTR_Apps/src/manga/MangaJ.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View, Image, FlatList} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const MangaJ = () => {
+  // penggunaan api
+  const [manga, setManga] = React.useState([]);
+
+  useEffect(() => {
+    getManga();
+  });
+
+  const getManga = async () => {
+    firestore()
+      .collection('Manga')
+      .get()
+      .then(querySnapshot => {
+        const manga = [];
+        querySnapshot.forEach(doc => {
+          console.log(doc.data());
+          // data tiap dokumen dimasukkan ke array state lalu di masukkan ke data flatlist
+          manga.push({
+            ...doc.data(),
+            key: doc.id,
+          });
+        });
+        setManga(manga);
+      });
+  };
+  // penggunaan api
+
   return (
-    <View style={{marginRight: 12, marginVertical: 10}}>
-      <View>
-        <Image
-          source={require('../image/manga1.png')}
-          style={{width: 110, height: 140}}></Image>
-      </View>
-      <Text
-        style={{
-          fontSize: 16,
-          fontWeight: 'bold',
-          color: 'black',
-          textAlign: 'center',
-        }}>
-        Manga
-      </Text>
+    <View>
+      <FlatList
+        data={manga}
+        renderItem={({item}) => (
+          <View style={{marginRight: 12, marginVertical: 10}}>
+            <View>
+              <Image
+                source={require('../image/manga1.png')}
+                style={{width: 110, height: 140}}></Image>
+            </View>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: 'bold',
+                color: 'black',
+                textAlign: 'center',
+              }}>
+              {item.title}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
